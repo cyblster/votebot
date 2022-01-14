da395924bf97d899654bf1d81335346054508b92
fadeevdan77 <97438735+serafimkatasonov76@users.noreply.github.com>
2022-01-14 18:57:15 +0500

    Update by cyblster

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import pymysql
 
 from flask import Flask, request
 from telebot import TeleBot, types
@@ -7,14 +8,31 @@ from telebot import TeleBot, types
 APP_URL = os.environ.get("app_url")
 APP_TOKEN = os.environ.get("app_token")
 
-WHITE_LIST = (418064835, )  # Telegram ID, кому доступны команды
+MYSQL_HOST = os.environ.get("mysql_host")
+MYSQL_USER = os.environ.get("mysql_user")
+MYSQL_PASSWORD = os.environ.get("mysql_password")
+MYSQL_DATABASE = os.environ.get("mysql_database")
+
+admin_list = [418064835, ]
+member_list = []
 
 server = Flask(__name__)
 bot = TeleBot(token=APP_TOKEN)
 
 
+def mysql_query(query: str):
+    with pymysql.connect(
+            host=MYSQL_HOST, user=MYSQL_USER, password=MYSQL_PASSWORD,
+            database=MYSQL_DATABASE
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+
+            return cursor.fetchall()
+
+
 @server.route(f"/{APP_TOKEN}", methods=["POST"])
-def webhook():
+def bot_webhook():
     json_string = request.get_data().decode("utf-8")
     update = types.Update.de_json(json_string)
     bot.process_new_updates([update])
@@ -24,7 +42,7 @@ def webhook():
 
 @bot.message_handler(commands=["start"], chat_types=["private"])
 def command_start(message):
-    if message.from_user.id in WHITE_LIST:
+    if message.from_user.id in admin_list:
         inline_keyboard = types.InlineKeyboardMarkup()
         inline_keyboard.add(
             types.InlineKeyboardButton(text="Начать голосование", callback_data="button_start"),
@@ -40,10 +58,34 @@ def command_start(message):
         )
 
     else:
-        pass
+        telegram_id = message.from_user.id
+        telegram_username = message.from_user.username
+        telegram_firstname = message.from_user.firstname
+        telegram_lastname = message.from_user.lastname
+
+        if not telegram_username:
+            telegram_username = "NULL"
+        if not telegram_firstname:
+            telegram_firstname = "NULL"
+        if not telegram_lastname:
+            telegram_lastname = "NULL"
+
+        mysql_query(
+            f"INSERT INTO users (telegram_id, telegram_username, telegram_firstname, telegram_lastname) "
+            f"VALUES ({telegram_id}, {telegram_username}, {telegram_firstname}, {telegram_lastname})"
+        )
 
 
 if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL + APP_TOKEN)
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+    for fetch in mysql_query("SELECT * FROM users"):
+        member_list.append({
+            fetch[0]: {  # Telegram ID
+                "telegram_username": fetch[1],
+                "telegram_firstname": fetch[2],
+                "telegram_lastname": fetch[3]
+            }
+        })
