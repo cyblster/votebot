7dfe91adc193e8e5e67ab9a3d48354577d5de7ec
fadeevdan77 <97438735+serafimkatasonov76@users.noreply.github.com>
2022-01-14 19:14:44 +0500

    Update by cyblster

--- bot.py
+++ bot.py
@@ -60,34 +60,46 @@ def command_start(message):
             reply_markup=inline_keyboard
         )
 
+    elif message.from_user.id not in member_list:
+        telegram_id = message.from_user.id
+        telegram_username = message.from_user.username
+        telegram_firstname = message.from_user.firstname
+        telegram_lastname = message.from_user.lastname
+
+        if not telegram_username:
+               telegram_username = "NULL"
+        if not telegram_firstname:
+              telegram_firstname = "NULL"
+        if not telegram_lastname:
+              telegram_lastname = "NULL"
+
+        mysql_query(
+            f"INSERT INTO `users` (`telegram_id`, `telegram_username`, `telegram_firstname`, `telegram_lastname`) "
+            f"VALUES ({telegram_id}, '{telegram_username}', '{telegram_firstname}', '{telegram_lastname}')",
+            commit=True
+        )
+
+        member_list.append({
+            telegram_id: {
+                 "telegram_username": telegram_username,
+                 "telegram_firstname": telegram_firstname,
+                 "telegram_lastname": telegram_lastname
+            }
+        })
+
+        bot.send_message(
+            chat_id=message.from_user.id,
+            text="Вы зарегистрировались!"
+        )
+
     else:
-        if message.from_user.id not in member_list:
-            telegram_id = message.from_user.id
-            telegram_username = message.from_user.username
-            telegram_firstname = message.from_user.firstname
-            telegram_lastname = message.from_user.lastname
+        bot.send_message(
+            chat_id=message.from_user.id,
+            text="Вы УЖЕ зарегистрировались!"
+        )
 
-            if not telegram_username:
-                telegram_username = "NULL"
-            if not telegram_firstname:
-                telegram_firstname = "NULL"
-            if not telegram_lastname:
-                telegram_lastname = "NULL"
 
-            mysql_query(
-                f"INSERT INTO users (telegram_id, telegram_username, telegram_firstname, telegram_lastname) "
-                f"VALUES ({telegram_id}, {telegram_username}, {telegram_firstname}, {telegram_lastname})",
 
-                commit=True
-            )
-
-            member_list.append({
-                telegram_id: {
-                    "telegram_username": telegram_username,
-                    "telegram_firstname": telegram_firstname,
-                    "telegram_lastname": telegram_lastname
-                }
-            })
 
 
 if __name__ == "__main__":
@@ -95,7 +107,7 @@ if __name__ == "__main__":
     bot.set_webhook(url=APP_URL + APP_TOKEN)
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
-    for fetch in mysql_query("SELECT * FROM users"):
+    for fetch in mysql_query("SELECT * FROM `users`"):
         member_list.append({
             fetch[0]: {  # Telegram ID
                 "telegram_username": fetch[1],
