ab5c0e17566236da89202a1fbe98332ef2b71f22
fadeevdan77 <97438735+serafimkatasonov76@users.noreply.github.com>
2022-01-14 17:32:50 +0500

    Update by cyblster

--- bot.py
+++ bot.py
@@ -13,6 +13,15 @@ server = Flask(__name__)
 bot = TeleBot(token=APP_TOKEN)
 
 
+@server.route(f"/{APP_TOKEN}", methods=["POST"])
+def webhook():
+    json_string = request.get_data().decode("utf-8")
+    update = types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+
+    return "!", 200
+
+
 @bot.message_handler(commands=["start"], chat_types=["private"])
 def command_start(message):
     if message.from_user.id in WHITE_LIST:
@@ -37,3 +46,4 @@ def command_start(message):
 if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL + APP_TOKEN)
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
