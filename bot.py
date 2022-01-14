eb23ae83327a94f22853221408c5f994a0be8108
fadeevdan77 <97438735+serafimkatasonov76@users.noreply.github.com>
2022-01-14 17:30:17 +0500

    Update by cyblster

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 import os
-import telebot
 
-from telebot import types
+from flask import Flask, request
+from telebot import TeleBot, types
 
+
+APP_URL = os.environ.get("app_url")
+APP_TOKEN = os.environ.get("app_token")
 
 WHITE_LIST = (418064835, )  # Telegram ID, кому доступны команды
 
-bot = telebot.TeleBot(token=os.environ.get("bot_token"))
+server = Flask(__name__)
+bot = TeleBot(token=APP_TOKEN)
 
 
 @bot.message_handler(commands=["start"], chat_types=["private"])
@@ -27,7 +31,9 @@ def command_start(message):
         )
 
     else:
+        pass
 
 
-
-bot.polling()
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL + APP_TOKEN)
