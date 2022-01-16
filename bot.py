d62c9a2ee8c0670af1e7d383b07cd98aab6a161f
fadeevdan77 <97813283+sadykovalisher21@users.noreply.github.com>
2022-01-17 02:13:47 +0500

    Update by cyblster

--- bot.py
+++ bot.py
@@ -140,12 +140,7 @@ def message_any(message):
                     db=MYSQL_DATABASE, autocommit=True
             ) as connection:
                 with connection.cursor() as cursor:
-                    cursor.execute(f"UPDATE `system` SET question = {message.text} WHERE id = 1")
-
-            bot.send_message(
-                chat_id=message.from_user.id,
-                text="Успешно!"
-            )
+                    cursor.execute(f"UPDATE `system` SET question = {message.text}")
 
         elif setting_answer_a_is_active:
             with pymysql.connect(
@@ -153,7 +148,7 @@ def message_any(message):
                     db=MYSQL_DATABASE, autocommit=True
             ) as connection:
                 with connection.cursor() as cursor:
-                    cursor.execute(f"UPDATE `system` SET answer_a = {message.text} WHERE id = 1")
+                    cursor.execute(f"UPDATE `system` SET answer_a = {message.text}")
 
         elif setting_answer_b_is_active:
             with pymysql.connect(
@@ -161,7 +156,7 @@ def message_any(message):
                     db=MYSQL_DATABASE, autocommit=True
             ) as connection:
                 with connection.cursor() as cursor:
-                    cursor.execute(f"UPDATE `system` SET answer_b = {message.text} WHERE id = 1")
+                    cursor.execute(f"UPDATE `system` SET answer_b = {message.text}")
 
 
 @bot.callback_query_handler(lambda call: True)
