307d80e288612520190a4ce3f1ea38f8c4d5af63
fadeevdan77 <97813283+sadykovalisher21@users.noreply.github.com>
2022-01-19 03:29:35 +0500

    Update by cyblster

--- bot.py
+++ bot.py
@@ -113,7 +113,7 @@ def app_result():
            "<title>Результаты голосования</title>" \
            "<style>" \
            "body {font-family: Roboto, sans-serif;}" \
-           "h2 {color: #003347; margin-bottom: 10px; font-weight: 700; font-size: 32px; line-height: 2.5;}" \
+           "h2 {color: #414a5f; margin-bottom: 10px; font-weight: 700; font-size: 32px; line-height: 2.5;}" \
            "h3 {color: #003347; margin-bottom: 10px; font-weight: 700; font-size: 24px; line-height: 2.5;}" \
            "ul {list-style: none; padding-left: 0; font-size: 16px;}" \
            "</style>" \
