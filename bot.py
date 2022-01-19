e9e86f434d07839d5d1d4602bbfba89d0cf3bf98
fadeevdan77 <97813283+sadykovalisher21@users.noreply.github.com>
2022-01-19 17:23:56 +0500

    Update by cyblster

--- bot.py
+++ bot.py
@@ -118,7 +118,7 @@ def app_result():
            "<meta charset='utf-8'>" \
            "<title>Результаты голосования</title>" \
            "<script>" \
-           "function loadUrl(){ window.location.href = '" + app_url + "'; }" \
+           "setInterval(function() {window.location.reload();}, 1000);" \
            "</script>" \
            "<style>" \
            "body {font-family: Roboto, sans-serif;}" \
