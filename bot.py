0a4b7557fb45cd96aa44cdd786bd87e55543baa9
fadeevdan77 <97813283+sadykovalisher21@users.noreply.github.com>
2022-01-19 16:18:37 +0500

    Update by cyblster

--- bot.py
+++ bot.py
@@ -136,9 +136,9 @@ def app_result():
            f"<span align='justify'>{question}</span>" \
            f"<h3 align='left'>Варианты ответа:</h3>" \
            f"<ul><li><div align='left'>А) {answer1} – <b>{count_answer1}</b></div>" \
-           f"<div class='bar_row'><div class='bar_result1' style='width: {answer1_procent if answer1_procent else '2'}%'><div class='bar_text'>{answer1_procent}%</div></div></div></li>" \
+           f"<div class='bar_row'><div class='bar_result1' style='width: {answer1_procent if answer1_procent else '3'}%'><div class='bar_text'>{answer1_procent}%</div></div></div></li>" \
            f"<li><div align='left'>Б) {answer2} – <b>{count_answer2}</b></div>" \
-           f"<div class='bar_row'><div class='bar_result2' style='width: {answer2_procent if answer2_procent else '2'}%'><div class='bar_text'>{answer2_procent}%</div></div></div></li></ul>" \
+           f"<div class='bar_row'><div class='bar_result2' style='width: {answer2_procent if answer2_procent else '3'}%'><div class='bar_text'>{answer2_procent}%</div></div></div></li></ul>" \
            f"</div></body>"
 
     return "<html>" + head + body + "</html>", 200
